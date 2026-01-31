90e559cc6bde0b2f5800e5cfb192d1fe3b8e24dd
Teofil-Razvan Ionita <teofilrazvanionita@gmail.com>
2026-01-31 07:19:45 +0200

    mutex definition to keep all the connections sync with each other

--- include/server.h
+++ include/server.h
@@ -4,4 +4,8 @@
 #define PORT "50000"   /* the PORT clients will be connectting to */
 #define BACKLOG 1024   /* how many pending connections are permitted */
 
+static pthread_mutex_t mtx = PTHREAD_MUTEX_INITIALIZER;
+
+
+
 #endif
