3625373e2771ed702ea967ff4ceb77c7706d5c4d
Teofil-Razvan Ionita <teofilrazvanionita@gmail.com>
2026-01-31 06:43:00 +0200

    preprocesor macros in the server module; using these constants in sockets' syscalls; all good with symbolls/back to normal coding

--- include/server.h
+++ include/server.h
@@ -1,5 +1,7 @@
 #ifndef __SERVER_H
 #define __SERVER_H
 
+#define PORT "50000"   /* the PORT clients will be connectting to */
+#define BACKLOG 1024   /* how many pending connections are permitted */
 
 #endif
