7d8e7fefbc7bc8184aa2c479d22815d3eeaa8b6d
Teofil-Razvan Ionita <teofilrazvanionita@gmail.com>
2026-02-02 15:52:30 +0200

    Optimizing code

--- include/config.h
+++ include/config.h
@@ -1,6 +1,9 @@
 #ifndef __CONFIG_H
 #define __CONFIG_H
 
+
+/* ToDo: Use it with inline procedures(functions) instead of static allocation
+ * 	for a better security */
 typedef struct{
 
 }CONFIG_DATA;
