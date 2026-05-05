33bed2d87d9d8be5323be5d284ade3a27903843e
Teofil-Razvan Ionita <teofilrazvanionita@gmail.com>
2026-05-05 09:04:28 +0300

    Verify modules build before expanding the project

--- include/server.h
+++ include/server.h
@@ -1,6 +1,8 @@
 #ifndef __SERVER_H
 #define __SERVER_H
 
+#include <pthread.h>
+
 #define PORT "50000"   /* the PORT clients will be connectting to */
 #define BACKLOG 1024   /* how many pending connections are permitted */
 
